c7ed828057113121f8a4eefbb45b25dc6cf8ff2e
Lígia kaylanne <142066909+ligiakaylanne@users.noreply.github.com>
2024-09-04 17:43:02 -0300

    atualização duvidas, rodapé, donwload da folha de redação

--- script-duvidas.js
+++ script-duvidas.js
@@ -1,28 +1,36 @@
 document.querySelectorAll('.faq-question').forEach((button, index) => {
     button.addEventListener('click', () => {
         const answer = button.nextElementSibling;
-        const icon = button.querySelector('figure');
+        const icon = button.querySelector('i');
         const parent = button.parentElement;
 
-        // Fechar todas as respostas e redefinir ícones e classes
-        document.querySelectorAll('.faq-answer').forEach((item) => {
-            item.classList.remove('show');
-        });
-
-        document.querySelectorAll('.faq-question').forEach((btn) => {
-            btn.classList.remove('active');
-            const icon = btn.querySelector('figure');
+        if (answer.classList.contains('show')) {
+            // Se a resposta já estiver aberta, simplesmente a feche
+            answer.classList.remove('show');
+            button.classList.remove('active');
             icon.style.transform = 'rotate(180deg)';
-        });
+            parent.classList.remove('highlight');
+        } else {
+            // Fechar todas as respostas e redefinir ícones e classes
+            document.querySelectorAll('.faq-answer').forEach((item) => {
+                item.classList.remove('show');
+            });
 
-        document.querySelectorAll('.faq-item').forEach((item) => {
-            item.classList.remove('highlight');
-        });
+            document.querySelectorAll('.faq-question').forEach((btn) => {
+                btn.classList.remove('active');
+                const icon = btn.querySelector('i');
+                icon.style.transform = 'rotate(180deg)';
+            });
 
-        // Mostrar apenas a resposta clicada e girar o ícone
-        answer.classList.toggle('show');
-        button.classList.toggle('active');
-        icon.style.transform = icon.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
-        parent.classList.toggle('highlight');
+            document.querySelectorAll('.faq-item').forEach((item) => {
+                item.classList.remove('highlight');
+            });
+
+            // Mostrar a resposta clicada e girar o ícone
+            answer.classList.add('show');
+            button.classList.add('active');
+            icon.style.transform = 'rotate(0deg)';
+            parent.classList.add('highlight');
+        }
     });
 });
